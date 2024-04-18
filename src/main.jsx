a8889e4d420ceda547878a7ec0682d6ec4ee2f01
otabek123 <solijonovotabek08@gmail.com>
2024-04-19 02:46:44 +0500

    otabek

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Props from './props'
+import "./index.css"
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <div>
+  <Props/>
+ </div>
+)
---
+++ src/props.jsx
@@ -0,0 +1,234 @@
+import { useState } from "react";
+import { AnimatePresence, easeOut, motion } from "framer-motion";
+
+const Props = () => {
+  return (
+    <div className="bg-[url('/bg-img.jpg')] bg-cover h-[100vh] w-full">
+      <div className="container mx-auto">
+        <header className="flex py-2 justify-between items-center">
+          <div className="logo">
+            <img
+              className="h-[60px] w-[140px] cursor-pointer"
+              src="/logo.png"
+              alt="img"
+            />
+          </div>
+          <nav className="flex justify-between items-center">
+            <FlyoutLink FlyoutContent={PricingContent2} href="#">
+              <a className="hover:bg-gray-300 px-[16px] py-[10px]" href="#">
+                vehicles
+              </a>
+            </FlyoutLink>
+            <FlyoutLink2 FlyoutContent={PricingContent} href="#">
+              <a className="hover:bg-gray-300 px-[16px] py-[10px]" href="#">
+                energy
+              </a>
+            </FlyoutLink2>
+            <FlyoutLink2 FlyoutContent={PricingContent} href="#">
+              <a className="hover:bg-gray-300 px-[16px] py-[10px]" href="#">
+                charging
+              </a>
+            </FlyoutLink2>
+            <FlyoutLink2 FlyoutContent={PricingContent} href="#">
+              <a className="hover:bg-gray-300 px-[16px] py-[10px]" href="#">
+                discover
+              </a>
+            </FlyoutLink2>
+            <FlyoutLink2 FlyoutContent={PricingContent} href="#">
+              <a className="hover:bg-gray-300 px-[16px] py-[10px]" href="#">
+                shop
+              </a>
+            </FlyoutLink2>
+          </nav>
+          <div className="flex items-center gap-2">
+            <img
+              className="h-[30px] w-[30px]"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/1200px-Icon-round-Question_mark.svg.png"
+              alt="img"
+            />
+            <img
+              className="h-[30px] w-[30px]"
+              src="https://seeklogo.com/images/G/globe-logo-42DE548AC7-seeklogo.com.png"
+              alt="img"
+            />
+            <div className="border border-black rounded-[50%]">
+              <img
+                className="h-[30px] w-[30px] rounded-[50%]"
+                src="https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
+                alt="img"
+              />
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+};
+// react  work
+const FlyoutLink = ({ children, href, FlyoutContent }) => {
+  const [open, setOpen] = useState(false);
+
+  const showFlyout = open && FlyoutContent;
+  return (
+    <div
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className="group relative h-fit w-fit"
+    >
+      <a
+        style={{ transform: "scaleX(0.5)" }}
+        className="text-black relative"
+        href={href}
+      >
+        {children}
+
+        {/* <span
+            style={{ transform: showFlyout ? "scaleX(1)" : "scaleX(0)" }}
+            className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left rounded-full bg-indigo-300 transition-transform duration-500 ease-out"
+          ></span> */}
+      </a>
+      <AnimatePresence>
+        {showFlyout && (
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 15 }}
+            style={{ x: "-50%" }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            className="absolute left-1/2 top-12 bg-white text-black"
+          >
+            <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent">
+              {" "}
+            </div>
+            <FlyoutContent />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+const FlyoutLink2 = ({ children, href, FlyoutContent }) => {
+  const [open, setOpen] = useState(false);
+
+  const showFlyout = open && FlyoutContent;
+  return (
+    <div
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className="group relative h-fit w-fit"
+    >
+      <a
+        style={{ transform: "scaleX(0.5)" }}
+        className="text-black relative"
+        href={href}
+      >
+        {children}
+
+        {/* <span
+          style={{ transform: showFlyout ? "scaleX(1)" : "scaleX(0)" }}
+          className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left rounded-full bg-indigo-300 transition-transform duration-500 ease-out"
+        ></span> */}
+      </a>
+      <AnimatePresence>
+        {showFlyout && (
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 15 }}
+            style={{ x: "-50%" }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            className="absolute left-1/2 top-12 bg-white text-black"
+          >
+            <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent">
+              {" "}
+            </div>
+            <FlyoutContent />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+// react  work
+
+// style work
+const PricingContent2 = () => {
+  return (
+    <div className="h-[500px] flex w-[1744px] justify-center gap-[50px] px-[200px] items-center bg-white p-6 shadow-xl">
+      <div className="flex flex-col items-start gap-[20px]">
+        <div className="flex gap-[10px]">
+          <div className="flex flex-col gap-[10px] items-center">
+            <img className="h-[123px] w-[200px]" src="/tesla.png" alt="img" />
+            <h1>Model S</h1>
+            <div className="flex gap-[30px]">
+              <p className="underline">Learn</p>
+              <p className="underline">order</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[10px] items-center">
+            <img className="h-[123px] w-[200px]" src="/tesla.png" alt="img" />
+            <h1>Model S</h1>
+            <div className="flex gap-[30px]">
+              <p className="underline">Learn</p>
+              <p className="underline">order</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[10px] items-center">
+            <img className="h-[123px] w-[200px]" src="/tesla.png" alt="img" />
+            <h1>Model S</h1>
+            <div className="flex gap-[30px]">
+              <p className="underline">Learn</p>
+              <p className="underline">order</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[10px] items-center">
+            <img className="h-[123px] w-[200px]" src="/tesla.png" alt="img" />
+            <h1>Model S</h1>
+            <div className="flex gap-[30px]">
+              <p className="underline">Learn</p>
+              <p className="underline">order</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-[20px]">
+          <div className="flex flex-col gap-[10px] items-center">
+            <img className="h-[123px] w-[200px]" src="/tesla.png" alt="img" />
+            <h1>Model S</h1>
+            <div className="flex gap-[30px]">
+              <p className="underline">Learn</p>
+              <p className="underline">order</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[10px] items-center">
+            <img className="h-[123px] w-[200px]" src="/tesla.png" alt="img" />
+            <h1>Model S</h1>
+            <div className="flex gap-[30px]">
+              <p className="underline">Learn</p>
+              <p className="underline">order</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-[35px] h-[fit]">
+        <div className="h-[] w-[1px] bg-gray-300"></div>
+        <div className="flex flex-col gap-[14px]">
+          <h1 className="hover:underline">Inventory</h1>
+          <h1 className="hover:underline">Used Cars</h1>
+          <h1 className="hover:underline">Demo Drive</h1>
+          <h1 className="hover:underline">Trade-in</h1>
+          <h1 className="hover:underline">Compare</h1>
+          <h1 className="hover:underline">Help Me Charge</h1>
+          <h1 className="hover:underline">Fleet</h1>
+          <h1 className="hover:underline">Semi</h1>
+          <h1 className="hover:underline">Roadster</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+// style work
+const PricingContent = () => {
+  return <div className="h-26  bg-white p-6 shadow-xl"></div>;
+};
+export default Props;
